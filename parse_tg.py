c5239e13a3ca9f176c9f1d1225147d5d5037a6a2
aleksandr <krainukovaleksandr1@gmail.com>
2025-07-30 15:54:05 +0300

    change parse func

--- parse_tg.py
+++ parse_tg.py
@@ -1,6 +1,5 @@
 import csv
 import os
-import tempfile
 from telethon import TelegramClient
 from typing import Optional
 from pathlib import Path
@@ -8,27 +7,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 
 client = TelegramClient('session_name', api_id, api_hash)
-
 
 async def async_parse_channel_to_csv(channel_username: str) -> Path:
     await client.start()
     try:
         messages = await client.get_messages(channel_username, limit=100)
 
-        temp = tempfile.NamedTemporaryFile(delete=False, suffix=".csv", mode='w', encoding='utf-8', newline='')
-        csv_path = Path(temp.name)
+        filename = f"{channel_username.strip('@')}.csv"
+        csv_path = Path.cwd() / filename
 
-        writer = csv.DictWriter(temp, fieldnames=["text"])
-        writer.writeheader()
-        for message in messages:
-            writer.writerow({'text': message.text or ''})
+        with open(csv_path, 'w', encoding='utf-8', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=["text"])
+            writer.writeheader()
+            for message in messages:
+                writer.writerow({'text': message.text or ''})
 
-        temp.close()
         return csv_path
 
     finally:
